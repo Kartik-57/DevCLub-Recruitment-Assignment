63e8c9770fdcc3112e75330dee4b003dc2617eb7
Kartik-57 <mt1200812@maths.iitd.ac.in>
2021-04-17 22:45:15 +0530

    added database, admin page, book model

--- PartI/LibrarySystem/Books/models.py
+++ PartI/LibrarySystem/Books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    Author = models.CharField(max_length=100)
+    Publisher = models.CharField(max_length=200)
+    Book_Title = models.CharField(max_length=200)
+    Genre = models.CharField(max_length=100) 
+    Book_Cover = models.CharField(max_length=1000)
+    ISBN = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.Book_Title + ' - ' + self.Author
+
+class Summary(models.Model):
+    Book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    
+
+
+    
